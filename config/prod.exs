0d9a0c9dc503efe0267dea347e9c7d4dcce3df17
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2020-10-25 18:56:27 +0100

    Set configuration for deployment

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,9 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :parafe, ParafeWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  http: [port: {:system, "PORT"}],
+  url: [scheme: "https", host: "powerful-sands-46187.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
