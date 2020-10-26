ca431f66c21f2555ed95394c70f97d2203a35335
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2020-10-26 08:27:23 +0100

    Remove root page and replace it with signature creation one

--- lib/parafe_web/router.ex
+++ lib/parafe_web/router.ex
@@ -17,8 +17,7 @@ defmodule ParafeWeb.Router do
   scope "/", ParafeWeb do
     pipe_through :browser
 
-    live "/", PageLive, :index
-    live "/sign", SignatureLive
+    live "/", SignatureLive
   end
 
   # Other scopes may use custom stacks.
