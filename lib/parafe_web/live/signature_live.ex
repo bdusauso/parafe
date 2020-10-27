721013f8af92fb6c7487a61c50fdcdbc611a0aaa
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2020-10-27 08:28:57 +0100

    Add basic logging

--- lib/parafe_web/live/signature_live.ex
+++ lib/parafe_web/live/signature_live.ex
@@ -1,6 +1,7 @@
 defmodule ParafeWeb.SignatureLive do
   use ParafeWeb, :live_view
   alias Parafe.{Crypto, Parameters, Signature, Signatures}
+  require Logger
 
   @impl true
   def mount(_params, _session, socket) do
@@ -15,6 +16,8 @@ defmodule ParafeWeb.SignatureLive do
 
   @impl true
   def handle_event("sign", %{"parameters" => params}, socket) do
+    Logger.info("Signing the request")
+
     signature =
       %Parameters{}
       |> Parameters.changeset(params)
@@ -29,12 +32,15 @@ defmodule ParafeWeb.SignatureLive do
       %Parameters{}
       |> Parameters.changeset(params)
       |> Map.put(:action, :insert)
+    Logger.info("Validate input parameters")
 
     {:noreply, assign(socket, input_params: params, enable_submit: params.valid?)}
   end
 
   @impl true
   def handle_event("generate_private_key", _, socket) do
+    Logger.info("Generate private key")
+
     key = Crypto.generate_private_key()
     socket = update(socket, :input_params, &Parameters.set_private_key(&1, key))
 
